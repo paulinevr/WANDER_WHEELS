5c718e85ec641483497b51066de2945b242551ad
Pauline van Rijckevorsel <vr.pauline@gmail.com>
2024-02-29 16:06:07 +0100

    add signup page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_202122) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_141502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_202122) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "profile_text"
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
